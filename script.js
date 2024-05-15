a262b0c93e4381187a6741d51001646d963a9c41
Kgomotso Leonard Nkosi <futurekgomotso@gmail.com>
2024-05-15 08:59:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let favoriteMovies = ['The nun II' , 'Saw X' , 'Moana' , 'Anyone but you' , 'Morbius' ];
+
+console.log(favoriteMovies[0]);
+console.log(favoriteMovies[4]);
+
+favoriteMovies.push(404);  //  to add something line here=====>>
+
+console.log("Try adding a movie title of a different data type, such as a number:", favoriteMovies);
+
+//Try adding a movie title of a different data type, such as a number
+
+
